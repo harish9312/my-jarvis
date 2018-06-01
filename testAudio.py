648ca2cd98f7b27412dd26036c1763626e428554
Harish <hksoni.9312@gmail.com>
2018-06-01 16:14:12 +0530

    Enhanced

--- testAudio.py
+++ testAudio.py
@@ -3,7 +3,13 @@ from gtts import gTTS
 from time import ctime
 import time 
 import os
+import sys
 
+f = open('/dev/null', 'w')
+os.stdout = f
+
+name = "Guest"
+n = 1
 def speak(audioString):
     print(audioString)
     tts = gTTS(text=audioString, lang='en')
@@ -14,17 +20,18 @@ def recordAudio():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Say Something")
-        print(source)
         audio = r.listen(source)
-        print(audio)
     data = ""
     try:
         data = r.recognize_google(audio)
         print("You Said: " + data)
     except sr.UnknownValueError :
         print("Cannot Understand")
-    except sr.RequestError as e:
+        # speak("Sorry I don't have an answer yet. But I will surely ask my Harish to make me able to answer you..!!!")
+    except sr.RequestError:
         print("Cannot understand")
+        # speak("Sorry I don't have an answer yet. But I will surely ask my Harish to make me able to answer you..!!!")
+
 
     return data
 
@@ -39,14 +46,18 @@ def jarvis(data):
         name = recordAudio()
         if(len(name) <= 0):
             speak("You did not gave me the name..!!!, Please say something...!!")
-            name =  recordAudio()
-        speak("Hurrayyy...!!! My name is "+ name)
+            assistantName =  recordAudio()
+        speak("Hurrayyy...!!! My name is "+ assistantName)
+    if "ok bye" in data:
+        speak("Nice taking with you")
+        n = 0
 
-print("Please enter your name first...!!!   ")
-name = input()
+speak("Can I know your name please?")
+name = recordAudio()
 time.sleep(2)
 speak("Hello "+name+", what can I do for you?")
-while 1:
+
+while n:
     data = recordAudio()
     print(data)
     jarvis(data)
